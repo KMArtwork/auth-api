94ea73f4827448b81f2e8dd4f7ca29400f7c7b4c
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-04-12 21:56:02 -0700

    alter test script

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -3,3 +3,10 @@
 const basic = require('../src/auth/middleware/basic')
 const bearer = require('../src/auth/middleware/bearer')
 const acl = require('../src/auth/middleware/acl')
+const jwt = require('jsonwebtoken');
+const base64 = require('base-64');
+const { usersDB, users } = require('../models/index')
+
+describe('Testing auth routes', () => {
+  
+})
